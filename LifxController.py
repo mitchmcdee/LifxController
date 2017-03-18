69844433279741ead6c536fe95e00c8e4894b763
Mitchell McDermott <mitchmcdee@users.noreply.github.com>
2017-03-19 01:59:52 +1000

    Update LifxController.py
    Added newlines for readability

--- LifxController.py
+++ LifxController.py
@@ -8,12 +8,14 @@ lights = {light.get_label(): light for light in LifxLAN().get_lights()}
 class LifxController(rumps.App):
     # Loop through all accessible lights.
     for name, light in lights.iteritems():
+        
         # Get the initial state of the light (on or off).
         initialState = 'ON' if light.get_power() else 'OFF'
 
         # Add light as a menu item and toggle its power when clicked.
         @rumps.clicked('{0} is {1}'.format(name, initialState))
         def toggle_power(self, sender):
+            
             # Get identifying label
             lightLabel = re.search('(.*) is O', sender.title).group(1)
 
