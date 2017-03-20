cd420e42d1d2bd4d325aaaf449e05170f066d674
mitchmcdee <mitchmcdee@gmail.com>
2017-03-21 03:20:44 +1000

    Mega update. Now automatically discovers new lights and updates light states. On/off status is now symbolised by a checkbox in the menuitem

--- LifxController.py
+++ LifxController.py
@@ -2,31 +2,63 @@ from lifxlan import *
 import rumps
 import re
 
-# Discovers all the LIFX lights and puts them in a dictionary with their labels.
-lights = {light.get_label(): light for light in LifxLAN().get_lights()}
+# Dictionary holding LIFX light names and their LIFX objects
+lights = {}
 
+# Timer event that keeps the list of active lights up to date
+@rumps.timer(10)
+def updateActiveLights():
+    global lights
+    lights = {light.get_label(): light for light in LifxLAN().get_lights()}
+
+# LIFX Controller class
 class LifxController(rumps.App):
-    # Loop through all accessible lights.
-    for name, light in lights.iteritems():
-        # Get the initial state of the light (on or off).
-        initialState = 'ON' if light.get_power() else 'OFF'
 
-        # Add light as a menu item and toggle its power when clicked.
-        @rumps.clicked('{0} is {1}'.format(name, initialState))
-        def toggle_power(self, sender):
-            # Get identifying label
-            lightLabel = re.search('(.*) is O', sender.title).group(1)
+    # Toggle light power when clicked
+    def toggle_power(self, sender):
+        # Get the associated light for the menu item
+        light = lights[sender.title]
 
-            # Get light state
-            lightState = re.search('ON', sender.title) != None
+        # No light was found, soft fail
+        if light is None:
+            return
 
-            # Toggle light power
-            lights[lightLabel].set_power(not lightState)
+        # Toggle state and power
+        sender.state = not sender.state
+        light.set_power(sender.state)
 
-            # Update text in the menu item title
-            oldState = 'ON' if lightState else 'OFF'
-            updatedState = 'OFF' if lightState else 'ON'
-            sender.title = sender.title.replace(oldState, updatedState)
+    # Add light as a clickable menu item
+    def addLightToMenu(self, name):
+        menuItem = rumps.MenuItem(name, callback=self.toggle_power)
+        menuItem.state = 0 # default light to display as off until updated
+        self.menu.insert_before('Quit', menuItem)
+
+    # Updates the on/off state for a single LIFX light
+    def updateState(self, name, light):
+        # Get the associated menu item for the light
+        menuItem = self.menu.get(name)
+
+        # No menu item was found, soft fail
+        if menuItem is None:
+            return
+
+        # Update the light's menu item state (light is on if power > 0)
+        menuItem.state = light.get_power() > 0
+
+    # Timer event that updates the menu with the active light's and their states
+    @rumps.timer(2)
+    def updateAllStates(self, _):
+        # Loops through the active lights and updates their on off state. Also
+        # will add newly discovered lights to the menu
+        # TODO(mitch): redesign to use sub menus? icons? http://bit.ly/2nKCbNR
+        for name, light in lights.iteritems():
+            if name not in self.menu:
+                self.addLightToMenu(name)
+            self.updateState(name, light)
 
 if __name__ == '__main__':
+    # Get active LIFX lights (first run only, timer will keep this updated)
+    updateActiveLights()
+
+    # Create LifxController menu and run it
     LifxController('LifxController', None, 'images/icon.png').run()
