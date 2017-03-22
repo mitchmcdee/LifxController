e9dce86c7c8e71bf3f2aa3f4c084f92e265f043c
mitchmcdee <mitchmcdee@gmail.com>
2017-03-23 02:09:53 +1000

    Somewhat working brightness sliders, refinement needed

--- LifxController.py
+++ LifxController.py
@@ -1,21 +1,16 @@
 from lifxlan import *
 import rumps
-import re
+import threading
+rumps.debug_mode(True)
 
-# Dictionary holding LIFX light names and their LIFX objects
+# Globally accessible dictionary holding LIFX light names and their LIFX objects
 lights = {}
-
-# Timer event that keeps the list of active lights up to date
-@rumps.timer(10)
-def updateActiveLights():
-    global lights
-    lights = {light.get_label(): light for light in LifxLAN().get_lights()}
 
 # LIFX Controller class
 class LifxController(rumps.App):
 
     # Toggle light power when clicked
-    def toggle_power(self, sender):
+    def togglePower(self, sender):
         # Get the associated light for the menu item
         light = lights[sender.title]
 
@@ -27,11 +22,22 @@ class LifxController(rumps.App):
         sender.state = not sender.state
         light.set_power(sender.state)
 
+    def hi(self, value):
+        lights['McDee\'s Bedroom'].set_brightness(value)
+
+    def onSliderUpdate(self, sender):
+        thread = threading.Thread(target=self.hi, args=(sender.value(), ))
+        thread.daemon = True                            # Daemonize thread
+        thread.start()
+        
+
     # Add light as a clickable menu item
     def addLightToMenu(self, name):
-        menuItem = rumps.MenuItem(name, callback=self.toggle_power)
+        menuItem = rumps.MenuItem(name, callback=self.togglePower)
         menuItem.state = 0 # default light to display as off until updated
+        slider = rumps.SliderMenuItem('Testing', callback=self.onSliderUpdate)
         self.menu.insert_before('Quit', menuItem)
+        self.menu.insert_before('Quit', slider)
 
     # Updates the on/off state for a single LIFX light
     def updateState(self, name, light):
@@ -45,8 +51,14 @@ class LifxController(rumps.App):
         # Update the light's menu item state (light is on if power > 0)
         menuItem.state = light.get_power() > 0
 
+    # Timer event that keeps the list of active lights up to date
+    @rumps.timer(20)
+    def updateActiveLights(self, _):
+        global lights
+        lights = {light.get_label(): light for light in LifxLAN().get_lights()}
+
     # Timer event that updates the menu with the active light's and their states
-    @rumps.timer(2)
+    @rumps.timer(20)
     def updateAllStates(self, _):
         # Loops through the active lights and updates their on off state. Also
         # will add newly discovered lights to the menu
@@ -57,8 +69,5 @@ class LifxController(rumps.App):
             self.updateState(name, light)
 
 if __name__ == '__main__':
-    # Get active LIFX lights (first run only, timer will keep this updated)
-    updateActiveLights()
-
     # Create LifxController menu and run it
-    LifxController('LifxController', None, 'icon.png').run()
+    LifxController('LifxController', None, 'icon.png', True).run()
